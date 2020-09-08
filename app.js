2b8429e0208d34778cffa48341889a582b03181f
Peter L-G <peter.larsson.green@gmail.com>
2020-09-08 11:48:33 +0200

    Added the humans page.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
+const humans = [{
+	id: 1,
+	name: "Alice",
+	age: 10
+}, {
+	id: 2,
+	name: "Bob",
+	age: 15
+}]
+
 const app = express()
 
 app.engine(".hbs", expressHandlebars({
@@ -13,6 +23,16 @@ app.get("/", function(request, response){
 
 app.get("/about", function(request, response){
 	response.render("about.hbs")
+})
+
+app.get("/humans", function(request, response){
+	
+	const model = {
+		humans
+	}
+	
+	response.render("humans.hbs", model)
+	
 })
 
 app.listen(8080)
