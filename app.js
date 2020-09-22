8b8830a2e8d536ec19edabbd6e68fbc6eb261069
Peter L-G <peter.larsson.green@gmail.com>
2020-09-22 12:41:44 +0200

    Added login functionality and hide things when not logged in.

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const sqlite3 = require('sqlite3')
+const expressSession = require('express-session')
+const SQLiteStore = require('connect-sqlite3')(expressSession)
 
 const HUMAN_NAME_MIN_LENGTH = 2
+const ADMIN_USERNAME = "admin"
+const ADMIN_PASSWORD = "abc123"
 
 const db = new sqlite3.Database("my-database.db")
 
@@ -45,6 +49,20 @@ app.use(express.static("static"))
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
+
+app.use(expressSession({
+	secret: "sdfkljwerlkjwer",
+	saveUninitialized: false,
+	resave: false,
+	store: new SQLiteStore({
+		db: "sessions.db"
+	})
+}))
+
+app.use(function(request, response, next){
+	response.locals.isLoggedIn = request.session.isLoggedIn
+	next()
+})
 
 app.get("/", function(request, response){
 	response.render("start.hbs")
@@ -244,6 +262,31 @@ app.get("/humans/:id", function(request, response){
 		}
 	})
 	
+})
+
+app.get("/login", function(request, response){
+	response.render("login.hbs")
+})
+
+app.post("/login", function(request, response){
+	
+	const enteredUsername = request.body.username
+	const enteredPassword = request.body.password
+	
+	if(enteredUsername == ADMIN_USERNAME && enteredPassword == ADMIN_PASSWORD){
+		request.session.isLoggedIn = true
+		response.redirect("/")
+	}else{
+		// TODO: Don't redirect, display error message instead.
+		response.redirect("/humans")
+	}
+	
+})
+
+// TODO: Logging out should be a POST request.
+app.get("/logout", function(request, response){
+	request.session.isLoggedIn = false
+	response.redirect("/")
 })
 
 app.listen(8080)
